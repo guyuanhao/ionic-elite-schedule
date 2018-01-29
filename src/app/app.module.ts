589214cd5cdeda533d574751914ea166aec06d17
Unknown <guyuanhao@foxmail.com>
2018-01-29 15:01:18 +0100

    initial env

---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+
+import { MyApp } from './app.component';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import {MyTeamsPage} from './../pages/pages';
+import { TournamentsPage } from './../pages/pages';
+import { TeamsPage } from './../pages/pages';
+import { GamePage } from './../pages/pages';
+import { TeamDetailPage } from './../pages/pages';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    MyTeamsPage,
+    TournamentsPage,
+    TeamsPage,
+    GamePage,
+    TeamDetailPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    MyTeamsPage,
+    TournamentsPage,
+    TeamsPage,
+    GamePage,
+    TeamDetailPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
